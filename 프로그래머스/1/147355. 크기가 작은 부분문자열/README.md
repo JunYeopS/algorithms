4d8dd87626e87d94a93047a009013428c45c8fbe
JunYeopS <127982951+JunYeopS@users.noreply.github.com>
2026-02-28 21:23:19 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 4.03 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크기가 작은 부분문자열 - 147355 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147355?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.45 MB, 시간: 5.31 ms
+메모리: 9.34 MB, 시간: 4.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 28일 21:14:48
+2026년 02월 28일 21:23:17
 
 ### 문제 설명
 
--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,18 +1,10 @@
 def solution(t, p):
-    t_len = len(t)
     p_len = len(p)
-    
-    ls = list(t)
     
     answer = 0
 
-    for i in range(t_len-(p_len-1)):
-        
-        tmp = (ls[i:p_len+i])
-        part_num = int("".join(tmp))
-        
-        if part_num <= int(p):
+    for i in range(len(t)-p_len+1):
+        if int(t[i:p_len+i]) <= int(p):
             answer += 1
-        
-    return answer
 
+    return answer
