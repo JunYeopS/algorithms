950d9d37f37b1f84772c803348f37e680701faed
JunYeopS <127982951+JunYeopS@users.noreply.github.com>
2026-03-04 19:26:08 +0900

    [Silver IV] Title: 대칭 차집합, Time: 224 ms, Memory: 89768 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -2,27 +2,11 @@ import sys
 
 input = sys.stdin.readline
 
-a_num,b_num = map(int,input().split())
+a,b = input().split()
 
-a_list = set(list(map(int,input().split())))
+a_list = set(map(int,input().split()))
+b_list = set(map(int,input().split()))
 
-b_list = set(list(map(int,input().split())))
+count = len(a_list ^ b_list)
 
-# a 에서 b와 교집합인것들 뺸다 
-# b에서 똑같이 
-# 각 갯수를 더한다 
-
-count = 0 
-for i in a_list:
-    if i in b_list:
-        continue
-    else:
-        count +=1 
-
-for i in b_list:
-    if i in a_list:
-        continue
-    else:
-        count +=1
-        
 print(count)
