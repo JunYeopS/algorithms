f067eb064f18b7472a1260d287971c377c847421
JunYeopS <127982951+JunYeopS@users.noreply.github.com>
2026-04-05 12:15:41 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -2,19 +2,9 @@ import sys
 
 input = sys.stdin.readline
 
-s = list(input().strip())
+s = (input().strip())
 
-count = 0
-for i in range(len(s)):
-    if s[i] == '=':
-        count -= 1
-        if s[i-2] == 'd' and s[i-1] == 'z':
-            count -=1 
-    elif s[i] == '-':
-        count -= 1
-    elif s[i] == 'j':
-        if s[i-1] == 'l' or s[i-1] == 'n':
-            count -= 1
-    count +=1 
-    
-print(count)
+for ch in ["dz=","z=","s=","c=","c-","d-","lj","nj"]:
+    s = s.replace(ch,"*")
+
+print(len(s))
